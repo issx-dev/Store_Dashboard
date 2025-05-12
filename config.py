c089d6bea3bb03fc15f7ccd323742c6d5656cb41
Issa El Mokadem <ielm0509@g.educaand.es>
2025-05-12 18:28:52 +0200

    Add config.py for MongoDB connection and data retrieval functions

---
+++ config.py
@@ -0,0 +1,32 @@
+# MODELS
+from models.Client import Client, ClientDB
+from models.Product import Product, ProductDB
+from models.Order import OrderDB
+
+# LIBRARIES
+from decouple import config
+
+# ENV VARIABLES
+MONGO_CONECTION_URL = config("MONGO_CONECTION_URL", cast=str)
+DATABASE_NAME = config("DATABASE_NAME", cast=str)
+
+
+def get_db_data(mongo_conection):
+    global products_data, users_data, categories_data, orders_data
+    # Database tables all data
+    products_data = [
+        ProductDB(*prod.values()) for prod in mongo_conection.Products.find({})
+    ]
+    users_data = [ClientDB(*usr.values()) for usr in mongo_conection.Users.find({})]
+    categories_data = mongo_conection.Categories.find_one()["Categories"]
+
+    orders_data = [
+        OrderDB(
+            order["_id"],
+            Client(**order["client"]),
+            [Product(**o) for o in order["products"]],
+        )
+        for order in mongo_conection.Orders.find({})
+    ]
+
+    return products_data, users_data, categories_data, orders_data
