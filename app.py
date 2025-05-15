ed5d67f125213077f65ff02627a07d394c016c4c
Issa El Mokadem <ielm0509@g.educaand.es>
2025-05-15 23:56:45 +0200

    Implement CRUD functionality for clients and enhance product management; add user interface for adding and viewing clients, update product templates, and improve error handling.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # DB MODELS
 from db.models.Product import Product, ProductDB
-from db.models.Client import ClientDB
+from db.models.Client import Client, ClientDB
 from db.models.Categorie import CategorieDB
 from db.models.Order import OrderDB
 from db.DataBase import DataBase
 
 # MODULES AND LIBRARIES
-from flask import Flask, Response, render_template, request, redirect
+from flask import Flask, Response, render_template, request, redirect, url_for
 from config import MONGO_CONECTION_URL, DATABASE_NAME
 from sample_data import admin_data
 
@@ -17,6 +17,7 @@ app = Flask(__name__)
 # MongoDB conection
 mongo_conection = DataBase(str(MONGO_CONECTION_URL), str(DATABASE_NAME))
 products_db = mongo_conection.db_tables("Products")[0]
+users_db = mongo_conection.db_tables("Users")[0]
 
 
 @app.route("/")
@@ -24,44 +25,41 @@ def home():
     return render_template("index.html", **admin_data)
 
 
+# PRODUCTS CRUD
+# All products page
 @app.route("/products")
 def products():
     data = mongo_conection.refresh_data([ProductDB], ["Products"])
     return render_template("products.html", products=data["Products"])
 
 
+# Get product info by id
 @app.route("/product/<id>")
 def product(id):
-    data = mongo_conection.refresh_data([ProductDB], ["Products"])
-    for product in data["Products"]:
+    data = mongo_conection.refresh_data([ProductDB], ["Products"])["Products"]
+    for product in data:
         if str(product.id) == id:
-            return render_template("product.html", product=product)
+            return render_template("product.html", prod=product)
 
-    return render_template("404.html", error="Product not found")
-
-
-@app.route("/clients")
-def clients():
-    data = mongo_conection.refresh_data([ClientDB], ["Users"])
-    users_data = data["Users"]
-    max_orders_client = max(users_data, key=lambda x: x.num_orders)
-    users_data.sort(key=lambda x: x.num_orders, reverse=True)
-    return render_template(
-        "clients.html", clients=users_data, top_client=max_orders_client
-    )
+    return render_template("404.html", error_msg="Product not found")
 
 
-@app.route("/orders")
-def orders():
-    data = mongo_conection.refresh_data([OrderDB], ["Orders"])
-    orders_data = data["Orders"]
-    return render_template(
-        "orders.html",
-        orders=orders_data,
-        total_earnings=sum(order.total_price for order in orders_data),
-    )
+# Delete product
+@app.route("/del-product/<id>")
+def del_product(id):
+    data = mongo_conection.refresh_data([ProductDB], ["Products"])["Products"]
+    for product in data:
+        if str(product.id) == id:
+            try:
+                products_db.delete_one({"_id": product.id})
+                return redirect(url_for("home"))
+            except Exception as e:
+                return render_template("404.html", error_msg=e)
+
+    return render_template("404.html", error_msg="Product not found")
 
 
+# Create product
 @app.route("/add-product", methods=["GET", "POST"])
 def add_product():
     data = mongo_conection.refresh_data(
@@ -72,13 +70,7 @@ def add_product():
 
     if request.method == "POST":
         try:
-            new_prod = Product(
-                request.form["name"],
-                float(request.form["price"]),
-                int(request.form["stock"]),
-                request.form["category"],
-                request.form["img_url"],
-            )
+            new_prod = Product(**request.form)  # type: ignore -> Type validation already done in Object Creation
             products_db.insert_one(new_prod._to_json())
 
         except ValueError as e:
@@ -86,12 +78,80 @@ def add_product():
                 f"Error al crear el producto: {e}. Asegúrate de que los valores sean correctos.",
                 status=400,
             )
-        return redirect("/add_product")
+        return redirect(url_for("add_product"))
 
     return render_template(
         "add_product.html",
         products=products_data,
         categories=cats_data[0].categories,
+    )
+
+
+# CLIENTS CRUD
+# All users page
+@app.route("/clients")
+def clients():
+    users_data = mongo_conection.refresh_data([ClientDB], ["Users"])["Users"]
+    max_orders_client = max(users_data, key=lambda x: x.num_orders)
+    users_data.sort(key=lambda x: x.num_orders, reverse=True)
+    return render_template(
+        "clients.html", clients=users_data, top_client=max_orders_client
+    )
+
+
+# Get client info by id
+@app.route("/client/<id>")
+def client(id):
+    data = mongo_conection.refresh_data([ClientDB], ["Users"])["Users"]
+    for client in data:
+        if str(client.id) == id:
+            return render_template("client.html", cli=client)
+
+    return render_template("404.html", error_msg="User not found")
+
+
+# Delete client
+@app.route("/del-client/<id>")
+def del_client(id):
+    data = mongo_conection.refresh_data([ClientDB], ["Users"])["Users"]
+
+    for client in data:
+        if str(client.id) == id:
+            try:
+                users_db.delete_one({"_id": client.id})
+                return redirect(url_for("home"))
+            except Exception as e:
+                return render_template("404.html", error_msg=e)
+
+    return render_template("404.html", error_msg="Product not found")
+
+
+# Create client
+@app.route("/add-client", methods=["GET", "POST"])
+def add_client():
+    if request.method == "POST":
+        try:
+            new_cli = Client(**request.form)  # type: ignore -> Type validation already done in Object Creation
+            users_db.insert_one(new_cli._to_json())
+
+        except ValueError as e:
+            return Response(
+                f"Error al crear el usuario: {e}. Asegúrate de que los valores sean correctos.",
+                status=400,
+            )
+        return redirect(url_for("add_client"))
+
+    return render_template("add_user.html")
+
+
+@app.route("/orders")
+def orders():
+    data = mongo_conection.refresh_data([OrderDB], ["Orders"])
+    orders_data = data["Orders"]
+    return render_template(
+        "orders.html",
+        orders=orders_data,
+        total_earnings=sum(order.total_price for order in orders_data),
     )
 
 
